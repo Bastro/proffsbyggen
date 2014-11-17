385983765b5a660470d8ed5a09f01db6011d15c0
Jonas Öster <jonas_leker@hotmail.com>
2014-11-17 13:28:59 +0100

    lägger till bcrypt och skapar ett test user object

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 // Moduler
 var Hapi = require('hapi'); // som import för java
+var Basic = require('hapi-auth-basic');
 var Path = require('path');
-// Server/Hapi inställningar
+var Bcrypt = require('bcrypt');
 var port = 3000;
+// Server/Hapi inställningar
 var server = new Hapi.Server('localhost', port, { files: { relativeTo: Path.join(__dirname, 'public') } }); // Skapar en server som "lyssnar" på port 3000
+
+// Skapar ett test object för att kolla validate
+var users = {
+    john: {
+        username: 'john',
+        password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm',   // 'secret'
+        name: 'John Doe',
+        id: '2133d32a'
+    }
+};
 
 
 // Add the server routes
