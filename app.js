691babc6d230db0990cc2c187725e17fe1516323
Jonas Öster <jonas_leker@hotmail.com>
2014-11-17 12:34:10 +0100

    lägger till en route för index.html

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // Moduler
 var Hapi = require('hapi'); // som import för java
+var Path = require('path');
 // Server/Hapi inställningar
 var port = 3000;
-var server = new Hapi.Server(port); // Skapar en server som "lyssnar" på port 3000
+var server = new Hapi.Server('localhost', port, { files: { relativeTo: Path.join(__dirname, 'public') } }); // Skapar en server som "lyssnar" på port 3000
 
+
+// Add the server routes
+//server.route(require('./server/routes')); //använder sen
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply.file('views/index.html');
+    }
+});
 /**
  * Startar servern
  * För att starta servern lär ni skriva node app.js i consolen och vara i rätt folder
@@ -11,6 +22,5 @@ var server = new Hapi.Server(port); // Skapar en server som "lyssnar" på port 3
 server.start(function () {
     console.log('Server running at:', server.info.uri);
 });
-
 
 // läs mer på http://hapijs.com/tutorials
