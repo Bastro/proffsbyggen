13aad772a3b6b1425c5ae9d766ba7adba2e05448
Jonas Öster <jonas_leker@hotmail.com>
2014-11-17 10:33:18 +0100

    uppdaterar lite information

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ var server = new Hapi.Server(port); // Skapar en server som "lyssnar" på port 3
 
 /**
  * Startar servern
+ * För att starta servern lär ni skriva node app.js i consolen och vara i rätt folder
  */
 server.start(function () {
     console.log('Server running at:', server.info.uri);
 });
+
+
+// läs mer på http://hapijs.com/tutorials
