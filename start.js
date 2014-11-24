d226ffb9797a0ee70324ea14b22dd3d3eeb59b6f
Jonas Öster <jonas_leker@hotmail.com>
2014-11-25 00:28:15 +0100

    bug fix

--- start.js
+++ start.js
@@ -29,7 +29,7 @@ server.views({
     engines: {
         html: require('swig')
     },
-    path: Path.join(__dirname, '../public/views')
+    path: Path.join(__dirname, './public/views')
 });
 
 module.exports = server;
