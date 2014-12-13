26861935a1dd32af09b2a3dd567077c5f3a9787e
Alexander Hagman <alexander.hagman94@gmail.com>
2014-12-13 14:36:09 +0100

    Revert "servertest"
    This reverts commit 474a8beaebec52a9b8d299f401b6ee80ab7600c4.

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ var pkg = require('../../package.json');
 
 var config = {};
 
-config.port = process.env.PORT || 80;
+config.port = process.env.PORT || 3000;
 
 // Exporterar config object så andra filer kan använda
 module.exports = config;
